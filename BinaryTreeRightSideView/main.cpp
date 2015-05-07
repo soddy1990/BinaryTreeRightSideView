c7180b3748a90ecf225739ae28803c3b1634e29d
soddy1990 <soddy1990@gmail.com>
2015-05-06 23:20:20 -0400

    accepted

--- BinaryTreeRightSideView/main.cpp
+++ BinaryTreeRightSideView/main.cpp
@@ -7,6 +7,48 @@
 //
 
 #include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+vector<int> rightSideView(TreeNode* root) {
+    vector<int> result;
+    queue<TreeNode> que;
+    if(root == NULL)
+        return result;
+    TreeNode* work;
+    work = root;
+    que.push(*work);
+    int countA = 1;
+    int countB = 0;
+    while(!que.empty()){
+        *work = que.front();
+        if(work->left != NULL){
+            que.push(*(work->left));
+            countB++;
+        }
+        if(work->right != NULL){
+            que.push(*(work->right));
+            countB++;
+        }
+        if(countA == 1){
+            result.push_back(work->val);
+            countA = countB;
+            countB = 0;
+        }else{
+            countA--;
+        }
+        que.pop();
+    }
+    return result;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
